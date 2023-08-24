8d97261110cb858308fa71605d5e909bc2df8aef
akari nari <lightakari7@gmail.com>
2023-08-24 17:35:20 +0000

    Easley

--- project1.py
+++ project1.py
@@ -1,1 +1,5 @@
-print("hello world")
+print("Welcome to the Space Travel Calculator")
+distance = int(input("Enter the distance to the desired celestial object in light-years."))
+speed = int(input("Enter the speed of your space craft in fraction of the speed of light"))
+time = distance/speed
+print("It would take", time, "hours to reach your destination in your space craft")
